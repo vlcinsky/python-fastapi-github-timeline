01b673e0151565d3f753dcc08c7a3b1500d9b6f7
Jan Vlcinsky <jan.vlcinsky@tamtamresearch.com>
2021-08-03 13:25:57 +0200

    Use oob to update "Fetch More" without duplicating indicators

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+GITHUB_REPOS_PER_PAGE = 30
 
 
 def format_date(val: str):
@@ -17,10 +18,16 @@ templates = Jinja2Templates(directory="templates")
 templates.env.filters["format_date"] = format_date
 
 
-def get_repos(user, page=1):
+def get_repos(user, page=1, per_page=GITHUB_REPOS_PER_PAGE):
     """Fetch info about GitHub user repos"""
+    print(f"get_repos: user: {user} page: {page}")
     url = f"https://api.github.com/users/{user}/repos"
-    params = {"sort": "created", "direction": "desc", "page": page}
+    params = {
+        "sort": "created",
+        "direction": "desc",
+        "page": page,
+        "per_page": per_page,
+    }
     return requests.get(url, params=params).json()
 
 
@@ -31,16 +38,32 @@ async def root(request: Request):
 
 @app.post("/repos", response_class=HTMLResponse)
 def repos(request: Request, user: str = Form(...)):
-    repos = get_repos(user)
+    page = 1
+    per_page = GITHUB_REPOS_PER_PAGE
+    repos = get_repos(user, page)
     return templates.TemplateResponse(
-        "_timeline.html", {"request": request, "user": user, "repos": repos}
+        "_timeline.html",
+        {
+            "request": request,
+            "user": user,
+            "repos": repos,
+            "page": page + 1,
+            "per_page": per_page,
+        },
     )
 
 
 @app.get("/more", response_class=HTMLResponse)
 def more(request: Request, user: str, page: int):
-    repos = get_repos(user, page)
+    per_page = GITHUB_REPOS_PER_PAGE
+    repos = get_repos(user, page, per_page)
     return templates.TemplateResponse(
-        "_timeline-secondary.html",
-        {"request": request, "user": user, "repos": repos, "page": page + 1},
+        "_timeline.html",
+        {
+            "request": request,
+            "user": user,
+            "repos": repos,
+            "page": page + 1,
+            "per_page": per_page,
+        },
     )
